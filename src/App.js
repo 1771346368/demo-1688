6f7ee5c003d4b7f4752b1602be5817deab618caf
dairui <1771346368@qq.com>
2024-04-01 09:54:59 +0800

    feat: 初始化项目与配置

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { CouponPage } from './container/CouponPage';
+
+function App() {
+  return (
+    <div style={{ backgroundColor: '#efefef' }}>
+      <CouponPage />
+    </div>
+  );
+}
+
+export default App;
